089ecbaf0fa762dd9506a5d11f9f92853b148732
Ia Vue <iavue00@gmail.com>
2023-02-02 21:35:00 -0600

    finally completed question 6

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -31,7 +31,18 @@ console.log(supplyChanges);
 //    - if the value is 0, log 'No Change.'
 //    - if the value is negative, format the log as 'Removed x parts.' 
 console.log('6. Showing supplyChanges...');
-
+for( let i=0; i<supplyChanges.length; i++){
+    console.log(i);
+    if(supplyChanges[i] > 0){
+        console.log('Added', supplyChanges[i] ,'parts.');
+    }
+    else if(supplyChanges[i] === 0){
+        console.log('No Change.');
+    }
+    else if(i>supplyChanges[i]){
+        console.log('Removed', supplyChanges[i] ,'parts.');
+    }
+}
 
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
