ce149a4ac38b2422e6381be43910e55d635ace98
Ia Vue <iavue00@gmail.com>
2023-02-04 15:40:20 -0600

    tried my best with stretch goals

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -48,11 +48,17 @@ for( let i=0; i<supplyChanges.length; i++){
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop. 
 console.log('7. Showing supplyChanges with "for of" loop');
-
+for(change of supplyChanges){
+    console.log(change);
+}
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
 console.log('8. Total supplies available is:');
-
+let total = 0;
+for(let i=0; i<supplyChanges.length; i+=1){
+    total += supplyChanges[i];
+}
+console.log(total);
 // 9. We have a large stash of parts in our warehouse that we 
 //    need to box up and get ready for shipment. 
 //    There are 572 parts in total, and each box holds 7 parts.
